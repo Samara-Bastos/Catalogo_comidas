d8c96e19296dda9b08145c9f4f3554b184328e06
Samara Bastos <samarabastos.dev@gmail.com>
2024-06-21 12:54:20 -0300

    inserção das imagens

--- src/componentes/quadro/imagens.js
+++ src/componentes/quadro/imagens.js
@@ -11,16 +11,118 @@ import img10 from '../../imagens/10.jpg';
 import img11 from '../../imagens/11.jpg';
 import img12 from '../../imagens/12.jpg';
 
-import imagemDefault from '../../imagens/imagem-default.jpg';
+import bolo01 from '../../imagens/bolo01.jpg';
+import bolo02 from '../../imagens/bolo02.jpg';
+import bolo03 from '../../imagens/bolo03.jpg';
+import bolo04 from '../../imagens/bolo04.jpg';
+import bolo05 from '../../imagens/bolo05.jpg';
+import bolo06 from '../../imagens/bolo06.jpg'; 
+import bolo07 from '../../imagens/bolo07.jpg';
+import bolo08 from '../../imagens/bolo08.jpg';
+import bolo09 from '../../imagens/bolo09.jpg';
+import bolo10 from '../../imagens/bolo10.jpg';
+import bolo11 from '../../imagens/bolo11.jpg';
+import bolo12 from '../../imagens/bolo12.jpg';
+
+import hotdog01 from '../../imagens/hotdog01.jpg';
+import hotdog02 from '../../imagens/hotdog02.jpg';
+import hotdog03 from '../../imagens/hotdog03.jpg';
+import hotdog04 from '../../imagens/hotdog04.jpg';
+import hotdog05 from '../../imagens/hotdog05.jpg';
+import hotdog06 from '../../imagens/hotdog06.jpg'; 
+import hotdog07 from '../../imagens/hotdog07.jpg';
+import hotdog08 from '../../imagens/hotdog08.jpg';
+import hotdog09 from '../../imagens/hotdog09.jpg';
+import hotdog10 from '../../imagens/hotdog10.jpg'; 
+import hotdog11 from '../../imagens/hotdog11.jpg';
+import hotdog12 from '../../imagens/hotdog12.jpg';
+
+import bebida01 from '../../imagens/bebida01.jpg';
+import bebida02 from '../../imagens/bebida02.jpg';
+import bebida03 from '../../imagens/bebida03.jpg';
+import bebida04 from '../../imagens/bebida04.jpg';
+import bebida05 from '../../imagens/bebida05.jpg';
+import bebida06 from '../../imagens/bebida06.jpg'; 
+import bebida07 from '../../imagens/bebida07.jpg';
+import bebida08 from '../../imagens/bebida08.jpg';
+import bebida09 from '../../imagens/bebida09.jpg';
+import bebida10 from '../../imagens/bebida10.jpg'; 
+import bebida11 from '../../imagens/bebida11.jpg';
+import bebida12 from '../../imagens/bebida12.jpg'; 
+
+import massa01 from '../../imagens/massa01.jpg';
+import massa02 from '../../imagens/massa02.jpg';
+import massa03 from '../../imagens/massa03.jpg';
+import massa04 from '../../imagens/massa04.jpg';
+import massa05 from '../../imagens/massa05.jpg';
+import massa06 from '../../imagens/massa06.jpg'; 
+import massa07 from '../../imagens/massa07.jpg';
+import massa08 from '../../imagens/massa08.jpg';
+import massa09 from '../../imagens/massa09.jpg';
+import massa10 from '../../imagens/massa10.jpg'; 
+import massa11 from '../../imagens/massa11.jpg';
+import massa12 from '../../imagens/massa12.jpg';  
+
+import pizza01 from '../../imagens/pizza01.jpg';
+import pizza02 from '../../imagens/pizza02.jpg';
+import pizza03 from '../../imagens/pizza03.jpg';
+import pizza04 from '../../imagens/pizza04.jpg';
+import pizza05 from '../../imagens/pizza05.jpg';
+import pizza06 from '../../imagens/pizza06.jpg'; 
+import pizza07 from '../../imagens/pizza07.jpg';
+import pizza08 from '../../imagens/pizza08.jpg';
+import pizza09 from '../../imagens/pizza09.jpg';
+import pizza10 from '../../imagens/pizza10.jpg'; 
+import pizza11 from '../../imagens/pizza11.jpg';
+import pizza12 from '../../imagens/pizza12.jpg'; 
+
+import pao01 from '../../imagens/pao01.jpg';
+import pao02 from '../../imagens/pao02.jpg';
+import pao03 from '../../imagens/pao03.jpg';
+import pao04 from '../../imagens/pao04.jpg';
+import pao05 from '../../imagens/pao05.jpg';
+import pao06 from '../../imagens/pao06.jpg'; 
+import pao07 from '../../imagens/pao07.jpg';
+import pao08 from '../../imagens/pao08.jpg';
+import pao09 from '../../imagens/pao09.jpg';
+import pao10 from '../../imagens/pao10.jpg'; 
+import pao11 from '../../imagens/pao11.jpg';
+import pao12 from '../../imagens/pao12.jpg'; 
+
+import cafe13 from '../../imagens/cafe13.jpg';
+import cafe02 from '../../imagens/cafe02.jpg';
+import cafe14 from '../../imagens/cafe14.jpg';
+import cafe04 from '../../imagens/cafe04.jpg';
+import cafe05 from '../../imagens/cafe05.jpg';
+import cafe06 from '../../imagens/cafe06.jpg'; 
+import cafe07 from '../../imagens/cafe07.jpg';
+import cafe08 from '../../imagens/cafe08.jpg';
+import cafe09 from '../../imagens/cafe09.jpg';
+import cafe10 from '../../imagens/cafe10.jpg'; 
+import cafe11 from '../../imagens/cafe11.jpg';
+import cafe12 from '../../imagens/cafe12.jpg'; 
+
+import frango01 from '../../imagens/frango01.jpg';
+import frango02 from '../../imagens/frango02.jpg';
+import frango03 from '../../imagens/frango03.jpg';
+import frango04 from '../../imagens/frango04.jpg';
+import frango05 from '../../imagens/frango05.jpg';
+import frango06 from '../../imagens/frango06.jpg'; 
+import frango07 from '../../imagens/frango07.jpg';
+import frango08 from '../../imagens/frango08.jpg';
+import frango09 from '../../imagens/frango09.jpg';
+import frango10 from '../../imagens/frango10.jpg'; 
+import frango11 from '../../imagens/frango11.jpg';
+import frango12 from '../../imagens/frango12.jpg'; 
 
 export const Imgs = [
     {img: [img01,img02,img03,img04,img05,img06,img07,img08,img09,img10,img11,img12]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]},
-    {img: [imagemDefault]}
+    {img: [bolo01, bolo02, bolo03, bolo04, bolo05, bolo06, bolo07, bolo08, bolo09, bolo10,bolo11, bolo12]},
+    {img: [hotdog01, hotdog02, hotdog03, hotdog04, hotdog05, hotdog06, hotdog07, hotdog08, hotdog09, hotdog10,hotdog11, hotdog12]},
+    {img: [bebida01, bebida02, bebida03, bebida04, bebida05, bebida06, bebida07, bebida08, bebida09, bebida10,bebida11, bebida12]},
+    {img: [massa01, massa02, massa03, massa04, massa05, massa06, massa07, massa08, massa09, massa10,massa11, massa12]},
+    {img: [pizza01, pizza02, pizza03, pizza04, pizza05, pizza06, pizza07, pizza08, pizza09, pizza10,pizza11, pizza12]},
+    {img: [pao01, pao02, pao03, pao04, pao05, pao06, pao07, pao08, pao09, pao10,pao11, pao12]},
+    {img: [cafe13, cafe02, cafe14, cafe04, cafe05, cafe06, cafe07, cafe08, cafe09, cafe10,cafe11, cafe12]},
+    {img: [frango01, frango02, frango03, frango04, frango05, frango06, frango07, frango08, frango09, frango10,frango11, frango12]}
 ]; 
